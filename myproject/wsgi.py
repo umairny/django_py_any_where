16764e48802b4ed53309eb142907b7c2779fe469
umairny <umairny1@gmail.com>
2022-11-26 18:46:27 -0500

    edit wsgi

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -14,3 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
+
+
+"""
+import os
+import sys
+
+path = os.path.expanduser('~/django_projects/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
+
+"""

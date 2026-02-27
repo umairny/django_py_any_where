86f2629665589b918f57f5f5bd99b5c5f1642931
umairny <umairny1@gmail.com>
2026-02-27 12:19:05 -0500

    db updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 """
 DATABASES = {
     'default': {
